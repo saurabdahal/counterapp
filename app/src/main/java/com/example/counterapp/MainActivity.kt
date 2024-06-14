b0e2f9389a4be63c87f192758204ca276c41f9b1
saurav dahal <d.saurab1@gmail.com>
2024-06-14 16:39:06 -0400

    initialized new project

---
+++ app/src/main/java/com/example/counterapp/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.counterapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.tooling.preview.Preview
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+        }
+    }
+}
+
